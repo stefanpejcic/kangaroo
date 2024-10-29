bff3a24f25861cb325eb767ed4244621137884f1
Stefan Pejcic <stefan@pejcic.rs>
2024-10-30 00:09:09 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,11 @@
 sudo apt update
 sudo apt install fzf
 
+
+
+
+# Ensure these lines are set as follows:
+PubkeyAuthentication yes
+AuthorizedKeysFile .ssh/authorized_keys
+
+sudo systemctl restart ssh
