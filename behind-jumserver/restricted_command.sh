923fa6fa51caf600bb80b13b338d871a08908bdb
Stefan Pejcic <stefan@pejcic.rs>
2025-05-22 10:17:32 +0200

    Update restricted_command.sh

--- behind-jumserver/restricted_command.sh
+++ behind-jumserver/restricted_command.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 # /usr/local/bin/restricted_command.sh
+LOG_TAG="ssh-restricted"
+
+
+log_command() {
+    local timestamp
+    timestamp=$(date '+%Y-%m-%d %H:%M:%S')
+    logger -t $LOG_TAG "[$timestamp] User: $(whoami) Command: $*"
+}
 
 # Check if a command was passed
 if [ $# -eq 0 ]; then
@@ -7,18 +15,19 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
+log_command "$@"
+
 # List of disabled commands
 disabled_commands=("rm" "shutdown" "reboot" "--delete")
 
-# Check all arguments against the list of disabled commands
 for arg in "$@"; do
     for cmd in "${disabled_commands[@]}"; do
         if [[ "$arg" == "$cmd" ]]; then
+            log_command "[BLOCKED] Attempted blocked command: $cmd"
             echo "Error: Command '$cmd' is disabled."
             exit 1
         fi
     done
 done
 
-# Allow all other commands
 exec "$@"
