5e61be14cd213cdb6dedd95c277bbb7751bf3501
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-02-25 12:24:35 +0100

    Update add_server.sh

--- server/add_server.sh
+++ server/add_server.sh
@@ -226,7 +226,7 @@ setup_ssh_for() {
     local users="$1"
 
 	# adds all users to jump-users group - TODO: https://www.youtube.com/watch?v=tw429JGL5zo&list=RDtw429JGL5zo
-	awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | xargs -I {} usermod -aG jump-users {}
+	awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | xargs -I {} usermod -aG jump-users,_tlog {}
 
 	if [[ "$users" == "all" ]]; then
 		users=$(awk -F: '$1 != "root" && $7 ~ /(\/bin\/(bash|sh|zsh))$/ {print $1}' /etc/passwd)
