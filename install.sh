b53caf1eec6a71f4dc93c76bca2f83c4dbf19121
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-03-03 11:32:03 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -83,6 +83,7 @@ if ! grep -q 'Kangaroo SSH JumpServer' /etc/ssh/sshd_config; then
 PubkeyAuthentication yes
 AuthorizedKeysFile .ssh/authorized_keys
 Match Group jump-users
+    ExposeAuthInfo yes
     ForceCommand ${SCRIPT_DIR}/server/client.sh
     AllowTcpForwarding no
     X11Forwarding no
